d76afb11bcd45c582cd0a3f4f59ad4a6edf77931
vtcw74g9vx-sketch <vtcw74g9vx@privaterelay.appleid.com>
2025-11-12 17:40:59 -0300

    Create Rodape.vue

---
+++ src/components/Rodape.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer>
+    <p>Com carinho, Turma 6 de Medicina — UNIVASF 💚</p>
+  </footer>
+</template>
+
+<style scoped>
+footer {
+  background-color: #004d40;
+  color: gold;
+  padding: 20px;
+  text-align: center;
+  font-weight: bold;
+}
+</style>
