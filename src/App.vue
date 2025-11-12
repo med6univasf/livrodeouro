0238dcf7fd28618abbbfe1aa38a70b0a5dbfc5c8
vtcw74g9vx-sketch <vtcw74g9vx@privaterelay.appleid.com>
2025-11-12 17:39:35 -0300

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Navbar />
+    <section>
+      <h1>Livro de Ouro — Medicina Turma 6 UNIVASF</h1>
+      <p>Espaço reservado para mensagem inicial e foto da turma 💚</p>
+    </section>
+
+    <Galeria />
+
+    <Rodape />
+  </div>
+</template>
+
+<script setup>
+import Navbar from './components/Navbar.vue'
+import Galeria from './components/Galeria.vue'
+import Rodape from './components/Rodape.vue'
+</script>
