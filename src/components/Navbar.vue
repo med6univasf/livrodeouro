ae7579ba33a4f2e83fc62f9fff6e1ae542f769e8
vtcw74g9vx-sketch <vtcw74g9vx@privaterelay.appleid.com>
2025-11-12 17:40:04 -0300

    Create Navbar.vue

---
+++ src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav class="navbar">
+    <h2>Med 6 UNIVASF</h2>
+  </nav>
+</template>
+
+<style scoped>
+.navbar {
+  position: fixed;
+  top: 0;
+  width: 100%;
+  background-color: #004d40;
+  color: gold;
+  padding: 10px 0;
+  text-align: center;
+  font-weight: bold;
+  z-index: 10;
+}
+</style>
